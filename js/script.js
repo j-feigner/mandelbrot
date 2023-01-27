d610084214cad25fc1d524a748ec2fe1c299a006
Jordan Feigner <jordan.feigner.r@gmail.com>
2023-01-27 01:51:58 -0800

    UI from Sierpinski

--- js/script.js
+++ js/script.js
@@ -21,6 +21,12 @@ function mandelbrot(c, ctx) {
         h: 2
     }
 
+    window.addEventListener("keydown", e => {
+        if(e.key = "ArrowLeft") {
+            viewportRect
+        }
+    })
+
     var canvasImageData = ctx.createImageData(2 * width, 2 * height);
 
     var pixelStep = viewportRect.w / canvasImageData.width;
@@ -35,12 +41,12 @@ function mandelbrot(c, ctx) {
         // Run Mandelbrot sequence on complex coordinate
         // Assign RGB value based on speed of divergence
         var c = new Complex(x, y);
-        var maxIterations = 1000;
+        var maxIterations = 100;
         var escapeNum = mandelbrotSeq(c, maxIterations);
         
         if(escapeNum != -1) {
             try {
-                var color = getRGB(maxIterations, escapeNum);
+                var color = getRGB(escapeNum);
                 canvasImageData.data[i + 0] = color.r;
                 canvasImageData.data[i + 1] = color.g;
                 canvasImageData.data[i + 2] = color.b;
@@ -66,8 +72,8 @@ function mandelbrot(c, ctx) {
 }
 
 function mandelbrotSeq(c, maxIterations) {
-    var step = new Complex(0, 0);
-    var maxMagnitude = 1000;
+    var step = c;
+    var maxMagnitude = 10;
     for(var j = 0; j < maxIterations; j++) {
         var zSquared = Complex.mult(step, step);
         var nextStep = Complex.add(zSquared, c);
@@ -82,8 +88,8 @@ function mandelbrotSeq(c, maxIterations) {
     return -1;
 }
 
-function getRGB(range, value) {
-    var categorySize = 18;
+function getRGB(value) {
+    var categorySize = 16;
     var category = Math.floor(value / categorySize) % 3;
 
     var colorVal = (value % categorySize) / categorySize;
@@ -114,6 +120,14 @@ function resetCanvasBitmap(canvas) {
     canvas.height = canvas.offsetHeight;
 }
 
+function setKeyEvents() {
+    window.addEventListener("keydown", e => {
+        if(e.key = "ArrowLeft") {
+
+        }
+    })
+}
+
 class Complex {
     constructor(a, b) {
         this.r = a;
@@ -132,5 +146,74 @@ class Complex {
 
     static mag(c) {
         return Math.sqrt(Math.pow(c.r, 2) + Math.pow(c.i, 2));
+    }
+}
+
+class Mandelbrot {
+    constructor() {
+        this.canvas = document.querySelector("#fractal-canvas");
+        this.ctx = this.canvas.getContext("2d");
+
+        this.viewportCoords = {
+            x: 0,
+            y: 0,
+            width: 0
+        };
+    }
+
+    render() {
+        // Dimensions of viewport in pixels
+        var width = c.width;
+        var height = c.height;
+
+        var viewportRect = {
+            x: -3.1,
+            y: 1.27,
+            w: 5,
+            h: 2
+        }
+
+        var canvasImageData = ctx.createImageData(2 * width, 2 * height);
+
+        var pixelStep = viewportRect.w / canvasImageData.width;
+        var pixel = 0;
+        for(var i = 0; i < canvasImageData.data.length; i += 4) {
+            // Get complex coordinate of pixel given viewport bounds
+            var row = Math.floor(pixel / canvasImageData.width);
+            var col = pixel % canvasImageData.width;
+            var x = viewportRect.x + (pixelStep * col);
+            var y = viewportRect.y - (pixelStep * row);
+
+            // Run Mandelbrot sequence on complex coordinate
+            // Assign RGB value based on speed of divergence
+            var c = new Complex(x, y);
+            var maxIterations = 1000;
+            var escapeNum = mandelbrotSeq(c, maxIterations);
+            
+            if(escapeNum != -1) {
+                try {
+                    var color = getRGB(escapeNum);
+                    canvasImageData.data[i + 0] = color.r;
+                    canvasImageData.data[i + 1] = color.g;
+                    canvasImageData.data[i + 2] = color.b;
+                    canvasImageData.data[i + 3] = 255;
+                } catch (error) {
+                    console.log(error + "Attempted escape number: " + escapeNum);
+                }
+            } else {
+                canvasImageData.data[i + 0] = 0;
+                canvasImageData.data[i + 1] = 0;
+                canvasImageData.data[i + 2] = 0;
+                canvasImageData.data[i + 3] = 255;
+            }
+
+            pixel++;
+        }
+
+        var renderCanvas = document.createElement("canvas");
+        renderCanvas.width = canvasImageData.width;
+        renderCanvas.height = canvasImageData.height;
+        renderCanvas.getContext("2d").putImageData(canvasImageData, 0 , 0);
+        ctx.drawImage(renderCanvas, 0, 0, width, height); 
     }
 }
